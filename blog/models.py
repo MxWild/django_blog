009ddb0bfe2beb7027f3b5a96bc07a8fa9393628
mxwild <mxwild@gmail.com>
2023-01-20 17:13:09 +0300

    slug fill automatically

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
 from blog.constants import STATUS
 
 
 class Post(models.Model):
     title = models.CharField(max_length=300, unique=True)
-    slug = models.SlugField(max_length=300, unique=True)
+    slug = models.SlugField(max_length=300, unique=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
@@ -16,6 +17,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-date_created']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
